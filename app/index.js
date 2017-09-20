2d97e2f1a536f418d24205530e1f735d9fbfd0f9
Corey <coreyn.martin@gmail.com>
2017-09-20 10:00:36 -0500

    Moves app div directly into index.html

--- app/index.js
+++ app/index.js
@@ -2,9 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {Container} from './components/Container'
 
-const body = document.getElementsByTagName('body')[0]
-const app = document.createElement('div')
-app.setAttribute('id', 'app')
-body.appendChild(app)
+const app = document.getElementById('app')
 
 ReactDOM.render(<Container/>, app)
